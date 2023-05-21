033d434355c1676ffc5b0b2af9ec7e91a93a3b61
sarfaraz187 <sarfarazmohammed187@gmail.com>
2023-05-21 21:34:24 +0200

    test 2

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <div>My Application works fine. Updated</div>
+    <div>My Application works fine. Updated 1</div>
   </React.StrictMode>
 );

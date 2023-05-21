34f407bfff24cc13a800a46b9237bfc32df489e6
sarfaraz187 <sarfarazmohammed187@gmail.com>
2023-05-21 20:28:47 +0200

    main.tsx file updated

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <div>This application has been updated 2</div>
+    <div>My Application works fine.</div>
   </React.StrictMode>
 );
